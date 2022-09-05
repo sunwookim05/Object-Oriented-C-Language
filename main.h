a3ce2c1ed0b279e423334cfc09aa7fb32f40c4ed
kimsunwoo <sunwookim05@gmail.com>
2022-09-05 19:20:03 +0900

    Master

--- main.h
+++ main.h
@@ -5,8 +5,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define import extern
 #define null NULL
-typedef enum{false, true} boolean;
 typedef char *String;
 typedef char int8_t;
 typedef short int16_t;
@@ -16,6 +16,8 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
+typedef uint8_t boolean;
+typedef enum __BOOL__{false, true} __BOOL__;
 
 void print(const String format, ...) {va_list ap;char buf[4096];va_start(ap, format);vsprintf(buf, format, ap);va_end(ap);fprintf(stdout, "%s", buf);}
 void println(const String format, ...) {va_list ap;char buf[4096];va_start(ap, format);vsprintf(buf, format, ap);va_end(ap);fprintf(stdout, "%s\n", buf);}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,1 @@
-
+    struct _IN{
