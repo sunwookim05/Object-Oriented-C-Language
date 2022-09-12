f0a93fb952908a6c0d7506584487cf62851b35fe
kimsunwoo <sunwookim05@gmail.com>
2022-09-12 23:16:40 +0900

    Master

--- unit_test.c
+++ unit_test.c
@@ -1,16 +1,27 @@
-#include "main.h"
+/* 
+    result
+    원본 선언 :0.008374
+    extern: 0.007954
+*/
+#include <stdio.h>
 #include <time.h>
 
 int main(void){
     double start, end;
+    int a;
+    double r;
 
     // import int plus(int, int);
     int plus(int a, int b);
 
-    start = (double)clock() / CLOCKS_PER_SEC;
-    printf("%d\n", plus(1, 2));
-    end = (double)clock() / CLOCKS_PER_SEC;
-    printf("time: %lf\n", (end - start));
+    for(int i = 0; i < 1000; i++){
+        start = ((double)clock() / CLOCKS_PER_SEC);    
+        a = plus(1, 2);
+        end = ((double)clock() / CLOCKS_PER_SEC);
+        a = 0;
+        r += (end - start);
+    }
+    printf("time: %lf\n", r);
 }
 
 int plus(int a, int b){
