849bf4334b77feab0842741f110f456179c0e956
kimsunwoo <sunwookim05@gmail.com>
2022-09-28 09:40:36 +0900

    Master

--- Scanner.c
+++ Scanner.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * @brief Get a char value from the user.
+ * @return char 
+ */
 char nextChar(void){
     char c;
     scanf("%c", &c);
@@ -7,6 +11,10 @@ char nextChar(void){
     return c;
 }
 
+/**
+ * @brief Get a int8_t value from the user.
+ * @return int8_t 
+ */
 int8_t nextByte(void){
     uint8_t b;
     scanf("%hhd", &b);
@@ -14,6 +22,10 @@ int8_t nextByte(void){
     return b;
 }
 
+/**
+ * @brief Get a int16_t value from the user.
+ * @return int16_t 
+ */
 int16_t nextShort(void){
     uint16_t s;
     scanf("%hd", &s);
@@ -21,6 +33,10 @@ int16_t nextShort(void){
     return s;
 }
 
+/**
+ * @brief Get a int32_t value from the user.
+ * @return int32_t 
+ */
 int32_t nextInt(void){
     uint32_t i;
     scanf("%d", &i);
@@ -28,6 +44,10 @@ int32_t nextInt(void){
     return i;
 }
 
+/**
+ * @brief Get a int64_t value from the user.
+ * @return int64_t
+ */
 int64_t nextLong(void){
     uint64_t l;
     scanf("%lld", &l);
@@ -35,6 +55,10 @@ int64_t nextLong(void){
     return l;
 }
 
+/**
+ * @brief Get a uint8_t value from the user.
+ * @return uint8_t 
+ */
 uint8_t nextUByte(void){
     uint8_t b;
     scanf("%hhu", &b);
@@ -42,6 +66,10 @@ uint8_t nextUByte(void){
     return b;
 }
 
+/**
+ * @brief Get a uint16_t from the user.
+ * @return uint16_t 
+ */
 uint16_t nextUShort(void){
     uint16_t s;
     scanf("%hu", &s);
@@ -49,6 +77,10 @@ uint16_t nextUShort(void){
     return s;
 }
 
+/**
+ * @brief Get a uint32_t value from the user.
+ * @return uint32_t 
+ */
 uint32_t nextUInt(void){
     uint32_t i;
     scanf("%u", &i);
@@ -56,6 +88,10 @@ uint32_t nextUInt(void){
     return i;
 }
 
+/**
+ * @brief Get a uint64_t value from the user.
+ * @return uint64_t
+ */
 uint64_t nextULong(void){
     uint64_t l;
     scanf("%llu", &l);
@@ -63,6 +99,10 @@ uint64_t nextULong(void){
     return l;
 }
 
+/**
+ * @brief Get a boolean value from the user
+ * @return boolean 
+ */
 boolean nextBoolean(void){
     String s = (String)malloc(sizeof(char) * 5);
     scanf("%s", s);
@@ -75,6 +115,10 @@ boolean nextBoolean(void){
     }
 }
 
+/**
+ * @brief Get a float number from the user.
+ * @return float
+ */
 float nextFloat(void){
     float f;
     scanf("%f", &f);
@@ -82,6 +126,10 @@ float nextFloat(void){
     return f;
 }
 
+/**
+ * @brief Get a double from the user.
+ * @return double 
+ */
 double nextDouble(void){
     double d;
     scanf("%lf", &d);
@@ -89,6 +137,10 @@ double nextDouble(void){
     return d;
 }
 
+/**
+ * @brief Get the next String object
+ * @return String 
+ */
 String next(void){
     String s = (String)calloc(0, sizeof(char) * 1000);
     scanf("%s", s);
@@ -97,6 +149,10 @@ String next(void){
     return str;
 }
 
+/**
+ * @brief Get the next Line object
+ * @return String 
+ */
 String nextLine(void){
     String s = (String)calloc(0, sizeof(char) * 1000);
     scanf("%[^\n]", s);
