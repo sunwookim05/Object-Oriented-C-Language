d4d65c9651a754050b7b1ec63a4d9104a2eed54e
kimsunwoo <sunwookim05@gmail.com>
2022-10-03 00:51:13 +0900

    Master

--- main.h
+++ main.h
@@ -22,15 +22,25 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
-typedef float float32_t;
-typedef double float64_t;
-typedef long double float128_t;
 typedef uint8_t boolean;
 
 #pragma pack(push, 1)
+/** 
+ * @brief System class
+ */ 
 typedef struct _System{
     struct _OUT_{
+        /**
+        * @brief System out print function
+        * @param format
+        * @return void
+        */
         void (*print)(const String, ...);
+        /** 
+        * @brief System out println function
+        * @param format
+        * @return void
+        */
         void (*println)(const String, ...);
     }out;
     struct __stdin_t{
@@ -40,21 +50,99 @@ typedef struct _System{
 #pragma pack(pop)
 
 #pragma pack(push, 1)
+/** 
+ * @brief Calculator class
+ */
 typedef struct Scanner{
+    /**
+    * @brief User input char
+    * @param in 
+    * @return char
+    */    
     char (*nextChar)(void);
+    /**
+     * @brief User input Byte
+     * @param in
+     * @return int8_t
+     */
     int8_t (*nextByte)(void);
+    /**
+     * @brief User input Short
+     * @param in
+     * @return int16_t
+     */
     int16_t (*nextShort)(void);
+    /**
+     * @brief User input Int
+     * @param in
+     * @return int32_t
+     */
     int32_t (*nextInt)(void);
+    /**
+     * @brief User input Long
+     * @param in
+     * @return int64_t
+     */
     int64_t (*nextLong)(void);
+    /**
+     * @brief User input Unsigned Byte
+     * @param in
+     * @return uint8_t
+     */
     uint8_t (*nextUByte)(void);
+    /**
+     * @brief User input Unsigned Short
+     * @param in
+     * @return uint16_t
+     */
     uint16_t (*nextUShort)(void);
+    /**
+     * @brief User input Unsigned Int
+     * @param in
+     * @return uint32_t
+     */
     uint32_t (*nextUInt)(void);
+    /**
+     * @brief User input Unsigned Long
+     * @param in
+     * @return uint64_t
+     */
     uint64_t (*nextULong)(void);
+    /**
+     * @brief User input boolean
+     * @param in
+     * @return boolean
+     */
     boolean (*nextBoolean)(void);
-    float32_t (*nextFloat)(void);
-    float64_t (*nextDouble)(void);
-    float128_t (*nextLDouble)(void);
+    /**
+     * @brief User input Float
+     * @param in
+     * @return float
+     */
+    float (*nextFloat)(void);
+    /**
+     * @brief User input Double
+     * @param in
+     * @return double
+     */
+    double (*nextDouble)(void);
+    /**
+     * @brief User input Long Double
+     * @param in
+     * @return long double
+     */
+    long double (*nextLDouble)(void);
+    /**
+     * @brief User input String no Spaces
+     * @param in
+     * @return String
+     */
     String (*next)(void);
+    /**
+     * @brief User input String with Spaces
+     * @param in
+     * @return String
+     */
     String (*nextLine)(void);
 }Scanner;
 #pragma pack(pop)
@@ -62,27 +150,93 @@ typedef struct Scanner{
 Scanner new_Scanner(struct __stdin_t);
 
 #pragma pack(push, 1)
+/** 
+ * @brief IO class
+ */
 typedef struct _IOBase{
+    /**
+    * @brief User input
+    * @param in 
+    * @return void
+    */
     void (*inPut)(double*, double*, char*);
+    /**
+    * @brief User output
+    * @param out
+    * @return void
+    */
     void (*outPut)(double, double, char, double, FILE*);
 }IOBase;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
+/** 
+ * @brief Calculator class
+ */
 typedef struct _Calculator{
     struct _IN{
+        /** 
+        * @brief User input
+        * @param in 
+        * @return void
+        */
         void (*inPut)(double*, double*, char*);
     }in;
     struct _OUT{
+        /** 
+         * @brief User output
+         * @param out
+         * @return void
+         */
         void (*outPut)(double, double, char, double, FILE*);
     }out;
+    /**
+     * @brief Request user reply
+     * @param Calculator
+     * @return boolean
+     */
     void (*requestReply)(boolean*);
+    /**
+     * @brief Calculate
+     * @param Calculator
+     * @return double
+     */
     double (*calculate)(double, double, char);
+    /**
+     * @brief Add
+     * @param Calculator
+     * @return double
+     */
     double (*plus)(double, double);
+    /**
+     * @brief Subtract
+     * @param Calculator
+     * @return double
+     */
     double (*minus)(double, double);
+    /**
+     * @brief Multiply
+     * @param Calculator
+     * @return double
+     */
     double (*multiply)(double, double);
+    /**
+     * @brief Divide
+     * @param Calculator
+     * @return double
+     */
     double (*divide)(double, double);
+    /**
+     * @brief Modulus
+     * @param Calculator
+     * @return double
+     */
     double (*mod)(double, double);
+    /**
+     * @brief Power
+     * @param Calculator
+     * @return double
+     */
     double (*pow)(double, double);
     struct _Calculator *this;
 }Calculator;
