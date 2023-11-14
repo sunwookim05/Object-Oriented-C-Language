2f6485bb66d7ea8574bdb9eb62d6c56b35da427b
kimsunwoo <sunwookim05@gmail.com>
2023-11-14 21:38:59 +0900

    test Code

--- src/main.c
+++ src/main.c
@@ -1,39 +1,13 @@
 #include "main.h"
-#include "Calculator.h"
 #include "System.h"
-
-//import void setUpProtected(Calculator*);
-
-//int main(void){
-//    FILE *fp;
-//    Calculator calculator = new_Calculator();
-//    boolean retry = true;
-//    double a, b, result;
-//    char op;
-//
-//    setUpProtected(&calculator);
-//    fp = fopen("log.txt", "w");
-//
-//    while(retry){
-//        calculator.in.inPut(&a, &b, &op);
-//        result = calculator.calculate(a, b, op);
-//        calculator.out.outPut(a, b, op, result, fp);
-//        retry = calculator.requestRetry();
-//    }
-//    fclose(fp);
-//
-//    return 0;
-//}
 
 import SYSTEM System;
 import INTEGER Integer;
 
-//TEST PLACE
 int main(void){
     String str = Integer.toString(Integer.parseInt("0xFF", 16));
     System.out.println(str);
 
     free(str);
-
     return 0;
 }
